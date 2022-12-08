ed9ce7cb77a7918afdde70f1712bff26eff15943
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-12-08 14:03:31 -0500

    done

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,13 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var userController = require('./controller/userController');
+var userController = require("./controller/userController");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.post('/sign-up', userController.signup);
+router.post("/users/sign-in", userController.signin);
 
-router.post('/sign-in', userController.signin);
+router.post("/sign-up", userController.signup);
+
+router.post("/sign-in", userController.signin);
 module.exports = router;
